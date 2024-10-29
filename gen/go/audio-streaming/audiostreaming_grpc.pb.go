3b7d386270104e56ebcfd82fb3e1aa23364ade04
DaniilZ77 <zolindaniil7705@gmail.com>
2024-10-29 18:59:13 +0300

    fix: removed streaming endpoint through grpc

--- gen/go/audio-streaming/audiostreaming_grpc.pb.go
+++ gen/go/audio-streaming/audiostreaming_grpc.pb.go
@@ -7,11 +7,7 @@
 package audiostreamingv1
 
 import (
-	context "context"
-	httpbody "google.golang.org/genproto/googleapis/api/httpbody"
 	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -19,15 +15,10 @@ import (
 // Requires gRPC-Go v1.64.0 or later.
 const _ = grpc.SupportPackageIsVersion9
 
-const (
-	AudioStreamingService_StreamAudio_FullMethodName = "/audiostreaming.AudioStreamingService/StreamAudio"
-)
-
 // AudioStreamingServiceClient is the client API for AudioStreamingService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type AudioStreamingServiceClient interface {
-	StreamAudio(ctx context.Context, in *StreamAudioRequest, opts ...grpc.CallOption) (grpc.ServerStreamingClient[httpbody.HttpBody], error)
 }
 
 type audioStreamingServiceClient struct {
@@ -38,30 +29,10 @@ func NewAudioStreamingServiceClient(cc grpc.ClientConnInterface) AudioStreamingS
 	return &audioStreamingServiceClient{cc}
 }
 
-func (c *audioStreamingServiceClient) StreamAudio(ctx context.Context, in *StreamAudioRequest, opts ...grpc.CallOption) (grpc.ServerStreamingClient[httpbody.HttpBody], error) {
-	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	stream, err := c.cc.NewStream(ctx, &AudioStreamingService_ServiceDesc.Streams[0], AudioStreamingService_StreamAudio_FullMethodName, cOpts...)
-	if err != nil {
-		return nil, err
-	}
-	x := &grpc.GenericClientStream[StreamAudioRequest, httpbody.HttpBody]{ClientStream: stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
-// This type alias is provided for backwards compatibility with existing code that references the prior non-generic stream type by name.
-type AudioStreamingService_StreamAudioClient = grpc.ServerStreamingClient[httpbody.HttpBody]
-
 // AudioStreamingServiceServer is the server API for AudioStreamingService service.
 // All implementations must embed UnimplementedAudioStreamingServiceServer
 // for forward compatibility.
 type AudioStreamingServiceServer interface {
-	StreamAudio(*StreamAudioRequest, grpc.ServerStreamingServer[httpbody.HttpBody]) error
 	mustEmbedUnimplementedAudioStreamingServiceServer()
 }
 
@@ -72,9 +43,6 @@ type AudioStreamingServiceServer interface {
 // pointer dereference when methods are called.
 type UnimplementedAudioStreamingServiceServer struct{}
 
-func (UnimplementedAudioStreamingServiceServer) StreamAudio(*StreamAudioRequest, grpc.ServerStreamingServer[httpbody.HttpBody]) error {
-	return status.Errorf(codes.Unimplemented, "method StreamAudio not implemented")
-}
 func (UnimplementedAudioStreamingServiceServer) mustEmbedUnimplementedAudioStreamingServiceServer() {}
 func (UnimplementedAudioStreamingServiceServer) testEmbeddedByValue()                               {}
 
@@ -96,17 +64,6 @@ func RegisterAudioStreamingServiceServer(s grpc.ServiceRegistrar, srv AudioStrea
 	s.RegisterService(&AudioStreamingService_ServiceDesc, srv)
 }
 
-func _AudioStreamingService_StreamAudio_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(StreamAudioRequest)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
-	}
-	return srv.(AudioStreamingServiceServer).StreamAudio(m, &grpc.GenericServerStream[StreamAudioRequest, httpbody.HttpBody]{ServerStream: stream})
-}
-
-// This type alias is provided for backwards compatibility with existing code that references the prior non-generic stream type by name.
-type AudioStreamingService_StreamAudioServer = grpc.ServerStreamingServer[httpbody.HttpBody]
-
 // AudioStreamingService_ServiceDesc is the grpc.ServiceDesc for AudioStreamingService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,12 +71,6 @@ var AudioStreamingService_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "audiostreaming.AudioStreamingService",
 	HandlerType: (*AudioStreamingServiceServer)(nil),
 	Methods:     []grpc.MethodDesc{},
-	Streams: []grpc.StreamDesc{
-		{
-			StreamName:    "StreamAudio",
-			Handler:       _AudioStreamingService_StreamAudio_Handler,
-			ServerStreams: true,
-		},
-	},
-	Metadata: "audio-streaming/audiostreaming.proto",
+	Streams:     []grpc.StreamDesc{},
+	Metadata:    "audio-streaming/audiostreaming.proto",
 }
